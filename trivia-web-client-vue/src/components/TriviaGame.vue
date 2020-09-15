ca181d8f4aa53b6b67264582af250b0d30ea4b91
Carole Cailleux <carole.cailleux@nivac.com>
2020-09-15 21:28:52 +0200

    added user list in vue + questions

---
+++ trivia-web-client-vue/src/components/TriviaGame.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="columns">
+        <div class="column" v-if="isLoading">Loading players...</div>
+        <div class="column" v-else>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Answers</th>
+                    <th>Points</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <template v-for="player in players">
+                    <tr v-bind:key="player.id">
+                        <td>{{player.id}}</td>
+                        <td>{{player.name}}</td>
+                        <td>{{player.answers}}</td>
+                        <td>{{player.points}}</td>
+                        <td>
+                        <button class="button is-primary" v-bind:class="{ 'is-loading' : isDeleting(player.id) }" @click="deletePlayer(player.id)">Delete Player</button>
+                        </td>
+                    </tr>
+                </template>
+            </tbody>
+        </table>
+        <player-form @completed="addPlayer"></player-form>
+        </div>
+        <div class="column">
+            <div class="card" v-if="question">
+                <div class="card-content">
+                    <button class="button is-primary" @click="getQuestion()">Refresh Question</button>
+                    <p class="title">
+                        
+                    </p>
+                    <p class="subtitle">
+                        {{this.question.question}}
+                    </p>
+                </div>
+                <footer class="card-footer">
+                    <p class="card-footer-item">
+                        <span>Correct answer: {{ this.question.answer }}</span>
+                    </p>
+                </footer>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { API_BASE_URL, TRIVIA_ENDPOINT } from '../config.js'
+import PlayerForm from './PlayerForm.vue'
+import Vue from 'vue'
+
+export default {
+    components: {
+        PlayerForm
+    },
+    data() {
+        return {
+            isLoading: true,
+            question: null,
+            players: {}
+        }
+    },
+    async created () {
+        this.getQuestion()
+        axios.defaults.headers.common['Authorization'] = `Bearer ${await this.$auth.getAccessToken()}`
+        try {
+            const response = await axios.get(API_BASE_URL + '/players')
+            this.players = response.data.data
+            console.log("players = ")
+            console.log(this.players)
+            this.isLoading = false
+        } catch (e) {
+            // handle the authentication error here
+        }
+    },
+    methods: {
+        async getQuestion() {
+            delete axios.defaults.headers.common.Authorization
+            this.doGetQuestion()
+            axios.defaults.headers.common['Authorization'] = `Bearer ${await this.$auth.getAccessToken()}`
+        },
+        async doGetQuestion() {
+            try {
+                const response = await axios.get(TRIVIA_ENDPOINT, {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded' 
+                    }
+                })
+                this.question = response.data[0]
+            } catch (e) {
+                // handle errors here
+            }
+        },
+        addPlayer(player) {
+            this.players.push(player)
+        },
+        isDeleting(id) {
+            let index = this.players.findIndex(player => player.id === id)
+            return this.players[index].isDeleting
+        },
+        async deletePlayer(id) {
+            let index = this.players.findIndex(player => player.id === id)
+            Vue.set(this.players[index], 'isDeleting', true)
+            await axios.delete(API_BASE_URL + '/players/' + id)
+            this.players.splice(index, 1)
+        }
+    }
+}
+</script>
--- trivia-web-client-vue/src/main.js
+++ trivia-web-client-vue/src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+import Dashboard from './components/Dashboard.vue'
+import Auth from '@okta/okta-vue'
+import TriviaGame from './components/TriviaGame.vue'
+
+Vue.use(Auth, {
+  issuer: 'https://myself-dev-729175.okta.com/oauth2/default',
+  client_id: '0oayjabe4JDjXU7ZA4x6',
+  redirect_uri: 'http://localhost:8080/implicit/callback',
+  scope: 'openid profile email'
+})
+
+const routes = [
+  { path: '/implicit/callback', component: Auth.handleCallback() },
+  { path: '/trivia', component: TriviaGame }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(Dashboard)
 }).$mount('#app')
